77b9a7d4045c83b1cab942d6c7b8500e324dd524
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-10-25 23:32:37 +0200

    initial sector implementation

---
+++ IFSCLAPI/VirtualStructures/Sector.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace IFSCLAPI.VirtualStructures
+{
+    public class Sector : ISector
+    {
+        public string Name { get; }
+        public List<ITower> Towers { get; }
+        public IVirtualWorld World { get; }
+
+        public Sector(string name, IVirtualWorld world , params ITower[] towers)
+        {
+            Name = name;
+            Towers = new List<ITower>();
+            Towers.AddRange(towers);
+            World = world;
+        }
+    }
+}
