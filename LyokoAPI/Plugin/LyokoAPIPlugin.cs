f62d6c4bcb9ca5d348d689d78a37f3bd786e79a0
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-11-02 15:22:54 +0100

    LyokoPlugin is an abstract class

--- LyokoAPI/Plugin/LyokoAPIPlugin.cs
+++ LyokoAPI/Plugin/LyokoAPIPlugin.cs
@@ -1,15 +1,55 @@
+using LyokoAPI.Events;
+
 namespace LyokoAPI.Plugin
 {
-    public interface LyokoAPIPlugin
+    public abstract class LyokoAPIPlugin
     {
-        string Name { get; }
-        string Author { get; }
-        bool Enabled { get; }
-        
-        bool OnEnable();
-        bool OnDisable();
+        public abstract string Name { get; }
+        public abstract string Author { get; }
+        public bool Enabled { get; private set; }
 
-        void OnGameStart(bool storyMode);
-        void OnGameEnd(bool failed);
+    
+        protected abstract bool OnEnable();
+        protected abstract bool OnDisable();
+
+        public bool Enable()
+        {
+            bool succeeded = OnEnable();
+            if (!succeeded)
+            {
+                Logger.Log("LyokoAPIPlugin",$"{ToString()} Didn't enable sucessfully");
+                Enabled = false;
+            }
+            else
+            {
+                Enabled = true;
+            }
+
+            return Enabled;
+        }
+        public bool Disable()
+        {
+            bool succeeded = OnDisable();
+            if (!succeeded)
+            {
+                Logger.Log("LyokoAPIPlugin",$"{ToString()} Didn't disable sucessfully");
+                Enabled = true;
+            }
+            else
+            {
+                Enabled = false;
+            }
+
+            return Enabled;
+        }
+        
+
+        public override string ToString()
+        {
+            return $"The plugin {Name} by {Author}";
+        }
+
+        public abstract void OnGameStart(bool storyMode);
+        public abstract void OnGameEnd(bool failed);
     }
 }
