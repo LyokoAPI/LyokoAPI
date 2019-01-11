cd99f218d4a1b5d55987db49647110e12d8b54e9
GoodOldJack12 <deloght.jonathan@gmail.com>
2019-01-11 23:41:01 +0100

    Added commandevents

---
+++ LyokoAPI/Events/CommandOutputEvent.cs
@@ -0,0 +1,74 @@
+using System.Reflection;
+
+namespace LyokoAPI.Events
+{
+    public class CommandOutputEvent
+    {
+        private static event Events.OnStringEvent stringE;
+        public static void Call(string command)
+        {
+            if (IsLocked && !Assembly.GetCallingAssembly().Equals(Events.Master))
+            {
+                return;
+            }
+
+            string finalcommand = command;
+            if (command.StartsWith("api."))
+            {
+                finalcommand = command.Substring(5);
+            }
+            
+            stringE?.Invoke(finalcommand);
+        }
+
+        public static Events.OnStringEvent Subscribe(Events.OnStringEvent func)
+        {
+            stringE += func;
+            return func;
+        }
+
+        public static void Unsubscribe(Events.OnStringEvent func)
+        {
+            stringE -= func;
+        }
+        
+        
+        
+        
+        
+        
+        
+        #region locking
+        private static bool _isLocked;
+        public static bool IsLocked
+        {
+            get => _isLocked || Events.AllLocked;
+            internal set => _isLocked = value;
+        }
+        /*
+         * Returns true if the lock was successful. 
+         */
+        public static bool Lock()
+        {
+            if (Events.hasMaster && !Assembly.GetCallingAssembly().Equals(Events.Master))
+            {
+                return false;
+            }
+            IsLocked = true;
+            return IsLocked;
+        }
+        /*
+         * Returns true if the unlock was successful
+         */
+        public static bool UnLock()
+        {
+            if (Events.hasMaster && !Assembly.GetCallingAssembly().Equals(Events.Master))
+            {
+                return false;
+            }
+            IsLocked = false;
+            return !IsLocked;
+        }
+        #endregion
+    }
+}
--- LyokoAPI/Events/Events.cs
+++ LyokoAPI/Events/Events.cs
@@ -46,6 +46,10 @@ namespace LyokoAPI.Events
         public delegate void OnLyokoEvent();
         public delegate void OnActivatorSwitch(ITower tower, APIActivator old,APIActivator newactivator);
 
-        public delegate void OnLogEvent(string message);
+        public delegate void OnLogEvent(string message); //Should properly deprecate this
+
+        public delegate void OnStringEvent(string message);
+
+
     }
 }
