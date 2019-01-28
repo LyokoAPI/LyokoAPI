abb20a91d9d08ebfb9335e24d83635227734021f
GoodOldJack12 <deloght.jonathan@gmail.com>
2019-01-28 13:58:20 +0100

    added dedicated version class

---
+++ LyokoAPI/API/Compatibility/CompatiblityLevel.cs
@@ -0,0 +1,12 @@
+namespace LyokoAPI.API
+{
+    public enum CompatiblityLevel : byte
+    {
+        None = 0,
+        MajorVersion,
+        MinorVersion,
+        Subversion,
+        Full,
+        
+    }
+}
