697421915ad704b3eb8f4b2d6a8eefbb996bcc88
GoodOldJack12 <deloght.jonathan@gmail.com>
2019-01-28 14:02:39 +0100

    Removed Version checking in Info

--- LyokoAPI/API/Info.cs
+++ LyokoAPI/API/Info.cs
@@ -9,49 +9,10 @@ namespace LyokoAPI.API
 {
     public static class Info
     {
-        public static string Version()
+        public static Version Version()
         {
             return typeof(Version).Assembly.GetName().Version.ToString();
         }
-
-        public static bool IsCompatible(string version)
-        {
-            var myVersion = ParseVersion(Info.Version());
-            var externalVersion = ParseVersion(version);
-
-            if (myVersion["MajorVersion"] == externalVersion["MajorVersion"])
-            {
-                //add exceptions here
-                return true;
-            }
-
-            return false;
-        }
-
-
-        private static IDictionary<string, int> ParseVersion(string version)
-        {
-            Dictionary<string,int> dictionary = new Dictionary<string, int>();
-            Stack<String> versions = new Stack<string>(version.Split('.'));
-            dictionary.Add("MajorVersion",0);
-            dictionary.Add("MinorVersion",0);
-            dictionary.Add("BugFix",0);
-            dictionary.Add("BuildVersion",0);
-
-            foreach (var keyValuePair in dictionary)
-            {
-                if (versions.Any())
-                {
-                    if (Int32.TryParse(versions.Pop(),out var versionNumber))
-                    {
-                        dictionary[keyValuePair.Key] = versionNumber;
-                    }
-                    
-                }
-            }
-
-            return dictionary;
-
-        }
+  
     }
 }
