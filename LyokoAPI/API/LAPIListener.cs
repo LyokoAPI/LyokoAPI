b1f1c2eb84e09c4e4ccaef9adf0caac4c9fdb63e
goodoldjack12 <deloght.jonathan@gmail.com>
2019-12-01 16:36:47 +0100

    Added a default listener

---
+++ LyokoAPI/API/LAPIListener.cs
@@ -0,0 +1,119 @@
+using LyokoAPI.Events;
+using LyokoAPI.Events.LWEvents;
+using LyokoAPI.VirtualEntities.LyokoWarrior;
+using LyokoAPI.VirtualStructures;
+using LyokoAPI.VirtualStructures.Interfaces;
+
+namespace LyokoAPI.API
+{
+    public abstract class LAPIListener
+    {
+
+        public void StartListening()
+        {
+            LW_DeathEvent.Subscribe(onLW_Death);
+            LW_MoveEvent.Subscribe(onLW_Move);
+            LW_FrontierEvent.Subscribe(onLW_Frontier);
+            LW_HurtEvent.Subscribe(onLW_Hurt);
+            LW_TranslationEvent.Subscribe(onLW_Trans);
+            LW_VirtEvent.Subscribe(onLW_Virt);
+            LW_DeathEvent.Subscribe(onLW_Devirt);
+            LW_HealEvent.Subscribe(onLW_Heal);
+            LW_XanaficationEvent.Subscribe(onLW_Xanafication);
+            TowerActivationEvent.Subscribe(onTowerActivation);
+            TowerDeactivationEvent.Subscribe(onTowerDeactivation);
+            TowerHijackEvent.Subscribe(onTowerHijack);
+            XanaAwakenEvent.Subscribe(onXanaAwaken);
+            XanaDefeatEvent.Subscribe(onXanaDefeat);
+        }
+
+        public void StopListening()
+        {
+            LW_DeathEvent.Unsubscribe(onLW_Death);
+            LW_MoveEvent.Unsubscribe(onLW_Move);
+            LW_FrontierEvent.Unsubscribe(onLW_Frontier);
+            LW_HurtEvent.Unsubscribe(onLW_Hurt);
+            LW_TranslationEvent.Unsubscribe(onLW_Trans);
+            LW_VirtEvent.Unsubscribe(onLW_Virt);
+            LW_DeathEvent.Unsubscribe(onLW_Devirt);
+            LW_HealEvent.Unsubscribe(onLW_Heal);
+            LW_XanaficationEvent.Unsubscribe(onLW_Xanafication);
+            TowerActivationEvent.Unsubscribe(onTowerActivation);
+            TowerDeactivationEvent.Unsubscribe(onTowerDeactivation);
+            TowerHijackEvent.Unsubscribe(onTowerHijack);
+            XanaAwakenEvent.Unsubscribe(onXanaAwaken);
+            XanaDefeatEvent.Unsubscribe(onXanaDefeat);
+        }
+
+
+
+
+        public virtual void onLW_Death(LyokoWarrior warrior)
+        {
+        }
+
+        public virtual void onLW_Devirt(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Frontier(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Heal(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Hurt(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Move(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Trans(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Virt(LyokoWarrior warrior)
+        {
+            
+        }
+
+        public virtual void onLW_Xanafication(LyokoWarrior warrior)
+        {
+        }
+
+        public virtual void onTowerActivation(ITower tower)
+        {
+            
+        }
+
+        public virtual void onTowerDeactivation(ITower tower)
+        {
+            
+        }
+
+        public virtual void onTowerHijack(ITower tower,APIActivator old, APIActivator newac)
+        {
+            
+        }
+
+        public virtual void onXanaAwaken()
+        {
+            
+        }
+
+        public virtual void onXanaDefeat()
+        {
+            
+        }
+    }
+}
